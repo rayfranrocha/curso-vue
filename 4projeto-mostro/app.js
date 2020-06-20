55f63b7cc45f6aaad4d5f9ab35b738dd1ec5ad87
rayfran.rocha <rayfran.rocha.lima@gmail.com>
2020-06-20 01:28:29 -0400

    Add score and buttons area

--- 4projeto-mostro/app.js
+++ 4projeto-mostro/app.js
@@ -1,7 +1,27 @@
 new Vue({
     el: "#app",
-    data: {},
-    computed: {},
+    data: {
+        running: false,
+        playerLife: 100,
+        monsterLife: 100,
+    },
+    computed: {
+        hasResult() {
+            return this.playerLife == 0 || this.monsterLife == 0
+        },
+        resultado() {
+            return this.playerLife == 0 ? "Você perdeu! :(" : "Você ganhou! :)"
+        }
+    },
     watch: {},
-    methods: {}
+    methods: {
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        giveUp() {
+            this.running = false
+        }
+    }
 })
