60a183fc6a32b858430ee03e89eaf6e6d8364226
rayfran.rocha <rayfran.rocha.lima@gmail.com>
2020-06-29 17:52:35 -0400

    Add desafio de BARRAMENTO para troca de dados intercomponentes

---
+++ 0desafios/desafio-06-comunicacao-desafio/src/components/UsuarioDetalhe.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="usuario-detalhe">
+        <h2 v-if="!usuario">Usuário não selecionado!</h2>
+        <div v-else>
+            <h2>ID: {{ usuario.id }}</h2>
+            <h2>Nome: {{ usuario.nome }}</h2>
+            <h2>Idade: {{ usuario.idade }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import barramento from '@/barramento'
+
+//perceba que o UsuarioDetalhe é só a saída, portanto não precisa de 'methods' 
+//ele precisa apenas do LISTENER definido no 'created()'
+
+export default {
+    data() {
+        return {
+            usuario: null
+        }
+    },
+    //created = funcao de ciclo de vida... onde nós definimos o LISTENER
+    created(){
+        barramento.onUsuarioSelecionado( usuario => {
+            this.usuario = usuario
+        })
+    }
+}
+</script>
+
+<style>
+    .usuario-detalhe {
+        flex: 1;
+        border: 1px solid #CCC;
+    }
+</style>
---
+++ 0desafios/desafio-06-comunicacao-desafio/src/components/UsuarioLista.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="usuario-lista">
+        <table>
+            <thead>
+                <tr>
+                    <th>#ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="usuario in usuarios" :key="usuario.id"
+                @click="setUsuarioSelecionado(usuario)">
+                    <td>{{ usuario.id }}</td>
+                    <td>{{ usuario.nome }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+
+import barramento from '@/barramento'
+
+//perceba que o UsuarioLista é a ENTRADA, portanto precisa de 'methods' 
+//ele NÃO precisa do LISTENER definido no 'created()'
+
+export default {
+    props: { usuarios: Array },
+    methods:{
+        setUsuarioSelecionado(usuario){
+            barramento.setUsuarioSelecionado(usuario)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .usuario-lista {
+        flex: 1;
+        display: flex;
+    }
+
+    table {
+        flex: 1;
+        border-spacing: 0px;
+        border-collapse: separate;
+        border-top: 1px solid #CCC;
+    }
+
+
+    th, td {
+        padding: 15px;
+        border-bottom: 1px solid #CCC;
+    }
+
+    tbody tr:hover {
+        background: rgba(82, 56, 196, 0.1);
+    }
+</style>
