0099c15fcf1d64cd727c51040317667f53d5f4d7
rayfran.rocha <rayfran.rocha.lima@gmail.com>
2020-07-02 21:36:39 -0400

    Add CSS of input and button

--- 7tarefas/src/components/NewTask.vue
+++ 7tarefas/src/components/NewTask.vue
@@ -1,26 +1,58 @@
 <template>
-  <div class="new-task" >
-     <input v-model="name" @keydown.enter="add" type="text">
-     <button @click="add">+</button>
+  <div class="new-task">
+    <input
+      v-model="name"
+      @keydown.enter="add"
+      type="text"
+      class="form-element"
+      placeholder="Nova tarefa?"
+    />
+    <button @click="add" class="form-element">+</button>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            name: ''
-        }
-    },
-    methods:{
-        add(){
-            this.$emit('taskAdded', {name : this.name})
-            this.name = ''
-        }
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods: {
+    add() {
+      this.$emit("taskAdded", { name: this.name });
+      this.name = "";
     }
+  }
 };
 </script>
 
 <style>
+.new-task {
+  margin: 35px;
+}
 
+.form-element {
+  outline: none;
+  font-size: 2rem;
+  border: 1px solid #fff;
+  padding: 5px 10px 8px;
+  color: #fff;
+}
+
+input.form-element {
+  width: 500px;
+  background: #fff2;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+button.form-element {
+  height: 52px;
+  width: 50px;
+  background: #2196f3;
+  border-left: none;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
 </style>
